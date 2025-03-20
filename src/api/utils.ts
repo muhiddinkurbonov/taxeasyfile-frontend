4f6061b445f5926f8a6fe24ff1d7ade1a228c8ba
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-03-20 18:53:19 -0500

    update

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { logout } from "./auth";
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api ";
 
 export const api = axios.create({
   baseURL: BASE_URL,
